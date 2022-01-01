3478b944816cc2ce546f556b848d7373925969ba
Ross Brown <rbrownwsws@googlemail.com>
2022-01-01 22:42:21 +0000

    Try to get postmap working

--- run.sh
+++ run.sh
@@ -10,10 +10,10 @@ set -e
 postconf -e "maillog_file=/dev/stdout"
 
 # Take environment variables and pass them to `postconf`
-# e.g. PF_MAIN_MYHOSTNAME=example.com -> `postconf -e "myhostname=example.com"`
+# e.g. POSTCONF_MAIN_MYHOSTNAME=example.com -> `postconf -e "myhostname=example.com"`
 
 # main.cf
-VAR_PREFIX="PF_MAIN_"
+VAR_PREFIX="POSTCONF_MAIN_"
 VAR_REGEX="^${VAR_PREFIX}\([A-Za-z_]\+\)=\(.*\)$"
 env | grep "^${VAR_PREFIX}" | while read -r ENV_VAR;
 do
@@ -21,6 +21,10 @@ do
     VAR_VALUE=$(echo "$ENV_VAR" | sed -e 's/'"${VAR_REGEX}"'/\2/')
     postconf -e "${VAR_NAME}=${VAR_VALUE}"
 done
+
+# Take environment variables and pass them to `postmap`
+# FIXME: Hardcoded just to test things.
+echo "${POSTMAP}" | postmap -i lmdb:/etc/postfix/virtual
 
 # Start postfix in the foreground
 exec /usr/sbin/postfix start-fg
