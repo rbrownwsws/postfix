cc855e0e5e37f03a7f994d291f242c825fa2402a
Ross Brown <rbrownwsws@googlemail.com>
2022-01-07 01:43:31 +0000

    feat: add ability to configure Master.cf with environment variables
    BREAKING CHANGE: POSTCONF_MAIN_xxx environment variables are renamed to
    POSTCONF_MAIN_PARAM_xxx

--- README.md
+++ README.md
@@ -6,21 +6,53 @@ A container image for running the [`postfix`](http://www.postfix.org/) MTA.
 
 You can configure `postfix` by passing envrionment variables to the container as described below.
 
-### Configuring `main.cf`
+### Configuring `main.cf` parameters
 
 Pass an environment variable to the container in this form:
 
-`POSTCONF_MAIN_<parameter>=<value>`
+`POSTCONF_MAIN_PARAM_<parameter>=<value>`
 
 > **Example**
-> 
+>
 > If you would normally configure the parameter by running:
-> 
+>
 > `postconf -e "myhostname=example.com"`
 >
 > You should pass the container this environment variable instead:
 >
-> `POSTCONF_MAIN_MYHOSTNAME=example.com`
+> `POSTCONF_MAIN_PARAM_MYHOSTNAME=example.com`
+
+### Configuring `master.cf` entries
+
+Pass an environment variable to the container in this form:
+
+`POSTCONF_MASTER_ENTRY_<service>_<type>=<value>`
+
+> **Example**
+>
+> If you would normally configure the entry by running:
+>
+> `postconf -M -e "smtp/inet=smtp inet n - n - - smtpd"`
+>
+> You should pass the container this environment variable instead:
+>
+> `POSTCONF_MASTER_ENTRY_SMTP_INET="smtp inet n - n - - smtpd"`
+
+### Configuring `master.cf` parameters
+
+Pass an environment variable to the container in this form:
+
+`POSTCONF_MASTER_PARAM_<service>_<type>_<parameter>=<value>`
+
+> **Example**
+>
+> If you would normally configure the parameter by running:
+>
+> `postconf -P -e "smtp/inet/smtpd_tls_security_level=may"`
+>
+> You should pass the container this environment variable instead:
+>
+> `POSTCONF_MASTER_PARAM_SMTP_INET_SMTPD_TLS_SECURITY_LEVEL=may`
 
 ### Postfix lookup tables
 
@@ -31,13 +63,13 @@ Pass an environment variable to the container in this form:
 _N.B. Files will be created in `/etc/postfix/<file_name>`_
 
 > **Example**
-> 
+>
 > If you normally had this file:
 >
 > >_/etc/postfix/virtual_
 > >
 > > @example.com john@gmail.com
-> 
+>
 > and would run:
 >
 > `postmap "lmdb:/etc/postfix/virtual"`
--- run.sh
+++ run.sh
@@ -3,36 +3,84 @@
 # Stop on errors
 set -e
 
-# Print commands for debugging
-[ "${DEBUG}" != "" ] && set -x
+# Print all commands for debugging
+[ -n "${DEBUG}" ] && set -x
 
 # Make postfix log to stdout
 postconf -e "maillog_file=/dev/stdout"
 
 # Take environment variables and pass them to `postconf`
-# e.g. POSTCONF_MAIN_MYHOSTNAME=example.com -> `postconf -e "myhostname=example.com"`
 
-# main.cf
-POSTCONF_PREFIX="POSTCONF_MAIN_"
-POSTCONF_REGEX="^${POSTCONF_PREFIX}\([A-Za-z_]\+\)=\(.*\)$"
-env | grep "^${POSTCONF_PREFIX}" | while read -r ENV_VAR;
+# main.cf parameters
+# e.g. POSTCONF_MAIN_PARAM_MYHOSTNAME=example.com -> `postconf -e "myhostname=example.com"`
+POSTCONF_MAIN_PARAM_PREFIX="POSTCONF_MAIN_PARAM_"
+POSTCONF_MAIN_PARAM_REGEX="^${POSTCONF_MAIN_PARAM_PREFIX}\([0-9A-Za-z_]\+\)=\(.*\)$"
+echo "### Main.cf Parameters ###"
+env | grep "^${POSTCONF_MAIN_PARAM_PREFIX}" | while read -r ENV_VAR;
 do
-    VAR_NAME=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_REGEX}"'/\1/' | tr '[:upper:]' '[:lower:]')
-    VAR_VALUE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_REGEX}"'/\2/')
-    postconf -e "${VAR_NAME}=${VAR_VALUE}"
+    PARAM=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MAIN_PARAM_REGEX}"'/\1/' | tr '[:upper:]' '[:lower:]')
+    VALUE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MAIN_PARAM_REGEX}"'/\2/')
+
+    RECORD="${PARAM}=${VALUE}"
+    echo "${RECORD}"
+    postconf -e "${RECORD}"
 done
+echo ""
+
+# master.cf service entries
+# e.g. POSTCONF_MASTER_ENTRY_SMTP_INET="smtp inet n - n - - smtpd" -> `postconf -M -e "smtp/inet=smtp inet n - n - - smtpd"`
+POSTCONF_MASTER_ENTRY_PREFIX="POSTCONF_MASTER_ENTRY_"
+POSTCONF_MASTER_ENTRY_REGEX="^${POSTCONF_MASTER_ENTRY_PREFIX}\([0-9A-Za-z]\+\)_\([0-9A-Za-z]\+\)=\(.*\)$"
+echo "### Master.cf Entries ###"
+env | grep "^${POSTCONF_MASTER_ENTRY_PREFIX}" | while read -r ENV_VAR;
+do
+    SERVICE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MASTER_ENTRY_REGEX}"'/\1/' | tr '[:upper:]' '[:lower:]')
+    TYPE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MASTER_ENTRY_REGEX}"'/\2/'  | tr '[:upper:]' '[:lower:]')
+    VALUE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MASTER_ENTRY_REGEX}"'/\3/')
+
+    RECORD="${SERVICE}/${TYPE}=${VALUE}"
+    echo "${RECORD}"
+    postconf -M -e "${RECORD}"
+done
+echo ""
+
+# master.cf service parameters
+# e.g. POSTCONF_MASTER_PARAM_SMTP_INET_SMTPD_TLS_SECURITY_LEVEL=may -> `postconf -P -e "smtp/inet/smtpd_tls_security_level=may"`
+POSTCONF_MASTER_PARAM_PREFIX="POSTCONF_MASTER_PARAM_"
+POSTCONF_MASTER_PARAM_REGEX="^${POSTCONF_MASTER_PARAM_PREFIX}\([0-9A-Za-z]\+\)_\([0-9A-Za-z]\+\)_\([0-9A-Za-z_]\+\)=\(.*\)$"
+echo "### Master.cf Parameters ###"
+env | grep "^${POSTCONF_MASTER_PARAM_PREFIX}" | while read -r ENV_VAR;
+do
+    SERVICE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MASTER_PARAM_REGEX}"'/\1/' | tr '[:upper:]' '[:lower:]')
+    TYPE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MASTER_PARAM_REGEX}"'/\2/'  | tr '[:upper:]' '[:lower:]')
+    PARAM=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MASTER_PARAM_REGEX}"'/\3/'  | tr '[:upper:]' '[:lower:]')
+    VALUE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTCONF_MASTER_PARAM_REGEX}"'/\4/')
+
+    RECORD="${SERVICE}/${TYPE}/${PARAM}=${VALUE}"
+    echo "${RECORD}"
+    postconf -P -e "${RECORD}"
+done
+echo ""
 
 # Take environment variables and pass them to `postmap`
-# e.g. POSTMAP_LMDB_VIRTUAL="@example.com john@gmail.com"-> `echo "@example.com john@gmail.com" | postmap -i lmdb:/etc/postfix/virtual`
+# e.g. POSTMAP_LMDB_VIRTUAL="@example.com john@gmail.com" -> `echo "@example.com john@gmail.com" | postmap -i lmdb:/etc/postfix/virtual`
 POSTMAP_PREFIX="POSTMAP_"
-POSTMAP_REGEX="^${POSTMAP_PREFIX}\([A-Za-z]\+\)_\([A-Za-z_]\+\)=\(.*\)$"
+POSTMAP_REGEX="^${POSTMAP_PREFIX}\([0-9A-Za-z]\+\)_\([0-9A-Za-z_]\+\)=\(.*\)$"
+echo "### Postmap Files ###"
 env | grep "^${POSTMAP_PREFIX}" | while read -r ENV_VAR;
 do
     FILE_TYPE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTMAP_REGEX}"'/\1/' | tr '[:upper:]' '[:lower:]')
     FILE_NAME=$(echo "$ENV_VAR" | sed -e 's/'"${POSTMAP_REGEX}"'/\2/' | tr '[:upper:]' '[:lower:]')
     FILE_VALUE=$(echo "$ENV_VAR" | sed -e 's/'"${POSTMAP_REGEX}"'/\3/')
-    echo "${FILE_VALUE}" | postmap -i "${FILE_TYPE}:/etc/postfix/${FILE_NAME}"
+
+    TABLE="${FILE_TYPE}:/etc/postfix/${FILE_NAME}"
+    echo "${TABLE}"
+    echo "${FILE_VALUE}" | postmap -i "${TABLE}"
 done
+echo ""
+
+echo "Starting postfix..."
+echo ""
 
 # Start postfix in the foreground
 exec /usr/sbin/postfix start-fg
